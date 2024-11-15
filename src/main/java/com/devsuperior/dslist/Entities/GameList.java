6a64b0531a52aa67e6f23c27a430136ed9542a3d
Lucas Muraro <lucas.muraro.pk@gmail.com>
2024-11-15 20:12:08 -0300

    domain model

---
+++ src/main/java/com/devsuperior/dslist/Entities/GameList.java
@@ -0,0 +1,59 @@
+package com.devsuperior.dslist.Entities;
+import java.util.Objects;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+// Mapeamento da tabela
+@Entity
+//Tabela Nome no banco de Dados
+@Table(name = "tb_game_list")
+public class GameList {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public GameList(){
+
+    }
+
+    public GameList(Long id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        GameList other = (GameList) obj;
+            return Objects.equals(id, other.id);
+    }
+}
